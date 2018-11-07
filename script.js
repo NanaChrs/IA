8f4d632e4efef9724037ad5e4653038b314b5dcf
Unknown <mathilde.christiaens@isen.yncrea.fr>
2018-11-07 15:49:46 +0100

    Stream vidéo qui fonctionne

--- script.js
+++ script.js
@@ -1,95 +1,77 @@
-var video=document.getElementById("videoElement");
+(function(){
 
-	if (navigator.mediaDevices.getUserMedia) {       
-	    navigator.mediaDevices.getUserMedia({video: true})
-	  .then(function(stream) {
-	    video.srcObject = stream;
-	    
-	  })
-	  .catch(function(err0r) {
-	    console.log("Something went wrong!");
-	  });
-	}
+	var filters =[{
+		name: "Blur",
+		filter:"blur(3px)"
+	},{
+		name: "Noir&Blanc",
+		filter:"grayscale(100%)"
+	},{
+		name:"Lumineux",
+		filter:"brightness(200%)"
+	},{
+		name:"Rotation",
+		filter:"hue-rotate(90deg)"
+	},{
+		name:"Inversion",
+		filter:"invert(100%)"
+	},{
+		name:"Saturé",
+		filter:"saturate(800%)"
+	},{
+		name:"Sepia",
+		filter:"sepia(400%)"
+	},{
+		name:"Contraste",
+		filter:"contrast(500%)"
+	},{
+		name:"Reset",
+		filter:""
+	}];
 
-var scene = new THREE.Scene();
+	var video=document.getElementById("video");
+	var canvas=document.getElementById("canvas");
+	var canvasContext = canvas.getContext("2d");
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+	//Permet d'utiliser le bon nom de fonction selon le navigateur utilisé
+	navigator.getUserMedia = (navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia ||
+      navigator.msGetUserMedia);
 
+	if (navigator.getUserMedia) {
+      function gotStream(stream) {
+        if (navigator.mozGetUserMedia) {
+          video.mozSrcObject = stream;
+        } else {
+          var vendorURL = window.URL || window.webkitURL;
+          video.src = vendorURL.createObjectURL(stream); 
+        }
 
-var animate = function () {
-	requestAnimationFrame( animate );
-
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
-};
-
-
-
-
-
-
-/*
-var lights = [];
-lights[ 0 ] = new THREE.PointLight( 0xff00ff, 1, 0 );
-lights[ 1 ] = new THREE.PointLight( 0xffff00, 1, 0 );
-lights[ 2 ] = new THREE.PointLight( 0x00ffff, 1, 0 );
-lights[ 0 ].position.set( 0, 200, 0 );
-lights[ 1 ].position.set( 100, 200, 100 );
-lights[ 2 ].position.set( - 100, - 200, - 100 );
-scene.add( lights[ 0 ] );
-scene.add( lights[ 1 ] );
-scene.add( lights[ 2 ] );*/
-
-function cubeanim() {
-	requestAnimFrame(cubeanim);
-	var positions = ctracker.getCurrentPosition();
-	var positionString = "";
-	var camera = new THREE.PerspectiveCamera( 75, video.videoWidth/video.videoHeight, 0.1, 1000 );
-	var renderer = new THREE.WebGLRenderer({alpha: true});
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	console.log(cube.position);
-		
-	if (positions){
-
-		camera.position.set(0,0,5);
-		camera.lookAt(scene.position);
-		camera.position.z = 3;
-		renderer.setSize( video.videoWidth, video.videoHeight);
-		document.body.appendChild(renderer.domElement);
-		// camera.position.set(cube.position.x,cube.position.y,15);
-		cube.position.set(positions[50][0]/300,positions[50][1]/300,0);
-}
-	renderer.render( scene, camera );
-	
-}
-
-var ctracker = new clm.tracker();
-ctracker.init();
-try {
-	ctracker.start(video)
-
-	
-	
-	cubeanim();
-}
-catch (err0r){
-	console.log(err0r);
-}
+        video.play();
+      }
+        
+      function error(message) {
+        console.log(message);
+      }
+      
+      function start() {
+        this.disabled = true;
+        navigator.getUserMedia( { 
+          audio: false,  
+          video: {
+            mandatory: {
+              maxWidth: 320,
+              maxHeight: 240
+            }
+          }
+        }, 
+        gotStream, 
+        error);
+      }
 
 
+    }
+    start();
 
-
-
-var canvasInput = document.getElementById('canvas');
-var cc = canvasInput.getContext('2d');
-function drawLoop() {
-	requestAnimFrame(drawLoop);
-	cc.clearRect(0, 0, canvasInput.width, canvasInput.height);
-	ctracker.draw(canvasInput);
-}
-//drawLoop();
+})();
